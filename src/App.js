b6fb645ef5c5617a1f8f176a66d3d758f451876c
Truman <trumanngny@yahoo.com>
2021-09-20 23:32:29 -0400

    added my personal image to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import logo from './1452646441575.jpeg';
 function App() {
   return (
     <div className="App">
